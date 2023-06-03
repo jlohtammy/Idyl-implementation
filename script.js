1e40441fd2533939b45615824920219af61bdee7
jlohtammy <tlloh@student.unimelb.edu.au>
2023-06-03 23:38:07 +1000

    redid side cart javascript

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function reveal() {
         x.style.display = "block";
   }
 }
+
+// SIDE CART
+const openBtn = document.getElementById('open_cart_btn')
+const cart = document.getElementById('sidecart')
+const closeBtn = document.getElementById('close_btn')
+
+openBtn.addEventListener('click', openCart)
+closeBtn.addEventListener('click', closeCart)
+
+// Open Cart
+function openCart(){
+    cart.classList.add('open')
+}
+
+// Close Cart
+function closeCart(){
+    cart.classList.remove('open')
+}
